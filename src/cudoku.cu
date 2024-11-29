1c02b2cdd5de44b2c32f0a1a0da731afb8d2aebb
joseppujadas <joseppujadas3@gmail.com>
2024-11-29 18:39:59 -0500

    making faster

--- src/cudoku.cu
+++ src/cudoku.cu
@@ -5,10 +5,10 @@
 #include "cudoku.h"
 #include "util.h"
 
-const int NUM_BLOCKS = 50000;
+const int NUM_BLOCKS = 1000;
 
 
-__global__ void solveBoard(char* boards, int* statuses, int* solution_idx, int* solution_found){
+__global__ void solveBoard(char* boards, int* statuses, int* solution_idx){
     
     //.Static shared memory for block constants
     __shared__ int progress_flag;
@@ -76,8 +76,13 @@ __global__ void solveBoard(char* boards, int* statuses, int* solution_idx, int* 
             if(val){
                 int old;
 
+                // row_possibles[threadIdx.x] |= mask;
+                // col_possibles[threadIdx.y] |= mask;
+                // inner_possibles[inner_idx] |= mask;
+
                 // Old & mask indicates the bit was masked BEFORE this update, so there
                 // is a conflict.
+ 
                 old = atomicOr(&row_possibles[threadIdx.x], mask);
                 if(old & mask){
                     error_flag = true;
@@ -136,7 +141,6 @@ __global__ void solveBoard(char* boards, int* statuses, int* solution_idx, int* 
         // Flag is set only when every cell has been filled
         if(done_flag){
             if(threadIdx.x + threadIdx.y == 0){
-                *solution_found = true;
                 *solution_idx = blockIdx.x * board_dim;
             }
             return;
@@ -147,7 +151,7 @@ __global__ void solveBoard(char* boards, int* statuses, int* solution_idx, int* 
         if(possible_ct != 0){
             atomicMin(&min_possibility_count, possible_ct);
         }
-        __syncthreads();
+        // __syncthreads();
 
         // Then find minimum cell index of those to update (arbitrary but fixed choice)
         if(possible_ct == min_possibility_count){
@@ -166,78 +170,99 @@ __global__ void solveBoard(char* boards, int* statuses, int* solution_idx, int* 
                     if(next_block_index != blockIdx.x){
                         
                         // next_block == 0 ? 1 : 0, i.e. atomic compare a block to 0 (idle) and set to 1 (working)
-                        while(next_block_index < NUM_BLOCKS && atomicCAS(&statuses[next_block_index], 0, 1) == 1)
+                        while(atomicCAS(&statuses[next_block_index], 0, 1) == 1)
                             next_block_index++;
                     }
 
-                    if(next_block_index < NUM_BLOCKS){
-                        char* new_board = &boards[next_block_index * board_size * board_size];
-                        memcpy(new_board, board, sizeof(char) * board_dim);
-                        new_board[ threadIdx.x * board_size + threadIdx.y] = possible;
 
-                        next_block_index++;
+                    char* new_board = &boards[next_block_index * board_size * board_size];
+
+                    if(next_block_index != blockIdx.x){
+                        memcpy(new_board, board, sizeof(char) * board_dim);
                     }
+                        
+                    new_board[ threadIdx.x * board_size + threadIdx.y] = possible;
+                    next_block_index++;
+                    
                 }
             }
         }
     }
 }
 
+// i genuinely dont know what thsi function does its 3am
+__global__ void solveBoardKernel(char* all_boards, int num_trials, int board_size,
+    char* boards_device, int* statuses, int* solution_idx){
+
+    dim3 gridDim(NUM_BLOCKS);
+    dim3 blockDim(board_size, board_size);
+    int status = 1;
+    int shared_memory_req = sizeof(int) * sqrtf(board_size) * 3;
+
+    for(int i = 0; i < num_trials; i++){
+        char* board = all_boards + (i * board_size * board_size);
+
+        memcpy(boards_device, board, board_size * board_size);
+        memcpy(statuses, &status, sizeof(int));
+
+        *solution_idx = 1;
+
+        while(*solution_idx == 1){
+            
+            solveBoard<<<gridDim, blockDim, shared_memory_req>>>(
+                boards_device, statuses, solution_idx
+            );
+            cudaDeviceSynchronize();
+        }
+        
+        memcpy(board, boards_device + *solution_idx, board_size * board_size);
+    }
+}
+
 double solveBoardHost(std::vector<std::vector<char>> boards){
 
+    int num_trials = boards.size();
     int board_size = boards[0].size();
+   
+    char* all_boards;
     char* boards_device;
     int* statuses;
-    int status = 1;
-
-    int solution_found = 0;
-    int* solution_found_device;
-    int solution_idx;
     int* solution_idx_device;
 
     // Allocate and initialize global memory
+    cudaMalloc(&all_boards, sizeof(char) * board_size * num_trials);
     cudaMalloc(&boards_device, sizeof(char) * board_size * NUM_BLOCKS);
     cudaMalloc(&statuses, sizeof(int) * NUM_BLOCKS);
-    cudaMalloc(&solution_found_device, sizeof(int));
     cudaMalloc(&solution_idx_device, sizeof(int));
+
+    std::vector<std::vector<char>> solutions(num_trials);
+
+    for(int i = 0; i < num_trials; ++i){
+        solutions[i].resize(board_size);
+
+        char* board = all_boards + (i * board_size);
+        cudaMemcpy(board, boards[i].data(), board_size, cudaMemcpyHostToDevice);
+    }
+
+    const auto compute_start = std::chrono::steady_clock::now();
+
+    solveBoardKernel<<<1, 1>>>(
+        all_boards, num_trials, sqrt(board_size), boards_device, 
+        statuses, solution_idx_device
+    );
     
+    cudaDeviceSynchronize();
+    const auto compute_time = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - compute_start).count();
 
-    dim3 blockDim(sqrt(board_size),sqrt(board_size));
-    dim3 gridDim(NUM_BLOCKS);
+    for(int i = 0; i < num_trials; ++i){
+        char* board = all_boards + (i * board_size);
+        cudaMemcpy(solutions[i].data(), board, board_size, cudaMemcpyDeviceToHost);
+    }  
 
-    int shared_memory_req = sizeof(int) * sqrt(board_size) * 3;
-
-    std::vector<char> solution(board_size);
-    double total_time = 0;
-    for(const auto& board : boards){
-        cudaMemset(statuses, 0, sizeof(int) * NUM_BLOCKS);
-        cudaMemcpy(statuses, &status, sizeof(int), cudaMemcpyHostToDevice);
-        solution_found = 0;
-        cudaMemcpy(solution_found_device, &solution_found, sizeof(int), cudaMemcpyHostToDevice);
-
-        
-        cudaMemcpy(boards_device, board.data(), sizeof(char) * board_size, cudaMemcpyHostToDevice);
-
-        // Call kernel in a loop to reschedule blocks until one finds a solution
-        while(!solution_found){
-            solveBoard<<<gridDim, blockDim, shared_memory_req>>>(
-                boards_device, statuses, solution_idx_device, solution_found_device
-            );
-            
-            const auto compute_start = std::chrono::steady_clock::now();
-            cudaMemcpy(&solution_found, solution_found_device, sizeof(int), cudaMemcpyDeviceToHost);
-            total_time += std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::steady_clock::now() - compute_start).count();
-        }
-        
-        // Copy board data back to host
-        cudaMemcpy(&solution_idx, solution_idx_device, sizeof(int), cudaMemcpyDeviceToHost);
-        cudaMemcpy(solution.data(), boards_device + solution_idx, board_size, cudaMemcpyDeviceToHost);
-        // const auto compute_time = 
-        
-        if(!verifySolve(board, solution)) printf("uh oh\n");
-
-       
+    cudaDeviceSynchronize();
+    for(int i = 0; i < num_trials; ++i){
+        verifySolve(boards[i], solutions[i]);
     }
     
-    return total_time;
+    return compute_time;
 }
